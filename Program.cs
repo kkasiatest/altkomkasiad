ffed798146e35c4059ac69e2bb96d7f9c77887df
Kasia <kkasiatest12@gmail.com>
2021-06-11 14:08:17 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace jenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine("Hi Kasia");
+            for(int i=0;i<10;i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(rand.Next(100,999));
+            }
+        }
+    }
+}
